2eb0967472793a3c41c3d645d01e6f89183adb2f
Giorgio Federici <federici.giorgio86@gmail.com>
2019-08-09 18:46:04 +0200

    Working on CD...

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/dist root@116.203.146.153:/root/production
+echo "Ciao!"
 ssh root@116.203.146.153 <<EOF
 cd production
 /usr/local/nvm/v11.14.0/lib/node_modules/pm2/bin/pm2 restart all

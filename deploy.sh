1507f79293455b6b78676dd0783df353327be0d2
Giorgio Federici <federici.giorgio86@gmail.com>
2019-08-09 19:47:42 +0200

    Working on CD...

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,8 @@
--#!/bin/bash
+#!/bin/bash
 set -e
-echo "Ciao!"
-echo $TRAVIS_BUILD_DIR
 rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/dist root@116.203.146.153:/root/production
 ssh root@116.203.146.153 <<EOF
- cd production/
- /usr/local/nvm/v11.14.0/lib/node_modules/pm2/bin/pm2 restart all
+ cd production/cd production/dist/server/bin
+ /usr/local/nvm/v11.14.0/lib/node_modules/pm2/bin/pm2 start ./server.js
  exit
 EOF

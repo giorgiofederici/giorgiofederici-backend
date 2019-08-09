755bc3d674f32e944ce9fb12c3ac98192bfe1fb5
Giorgio Federici <federici.giorgio86@gmail.com>
2019-08-09 22:25:41 +0200

    Working on CD...

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ rsync --quiet package.json root@116.203.146.153:/root/production
 ssh root@116.203.146.153 <<EOF
  cd /root/production
  npm install
- /usr/local/nvm/v11.14.0/lib/node_modules/pm2/bin/pm2 start /root/production/dist/server/bin/server.js
+ /usr/local/nvm/v11.14.0/lib/node_modules/forever/bin/forever stopall
+ /usr/local/nvm/v11.14.0/lib/node_modules/forever/bin/forever /root/production/dist/server/bin/server.js
  exit
 EOF

8e4b6afd4b46283403acc2157f9d21705701c35b
Giorgio Federici <federici.giorgio86@gmail.com>
2019-08-10 14:50:11 +0200

    deploy sh fixed after Meo

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 set -e
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/dist root@116.203.146.153:/root/production
-rsync --quiet package.json root@116.203.146.153:/root/production
-ssh root@116.203.146.153 <<EOF
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/dist root@$BACKEND_HOST:/root/production
+rsync --quiet package.json root@$BACKEND_HOST:/root/production
+ssh root@$BACKEND_HOST <<EOF
  cd /root/production
  npm install
- /usr/local/nvm/v11.14.0/lib/node_modules/forever/bin/forever stopall
- /usr/local/nvm/v11.14.0/lib/node_modules/forever/bin/forever start /root/production/dist/server/bin/server.js
+ /usr/local/nvm/v11.14.0/lib/node_modules/pm2/bin/pm2 reload all
  exit
 EOF
